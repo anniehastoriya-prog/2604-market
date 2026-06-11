b6ca11f6e3b2addb2500169b9720dc58e580ef89
Annie <anniehastoriya@gmail.com>
2026-06-11 02:27:29 -0500

    code has been written for the four files in the queries folder

---
+++ db/queries/orders.js
@@ -0,0 +1,49 @@
+import db from "#db/client";
+
+export async function createOrders(date, note, userId) {
+  const sql = `
+  INSERT INTO orders
+    (name, description, user_id)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *
+  `;
+  const {
+    rows: [order],
+  } = await db.query(sql, [date, note, userId]);
+  return order;
+}
+
+export async function getOrdersByUserId(id) {
+  const sql = `
+  SELECT *
+  FROM orders
+  WHERE user_id = $1
+  `;
+  const { rows: orders } = await db.query(sql, [id]);
+  return orders;
+}
+
+export async function getOrderById(id) {
+  const sql = `
+  SELECT *
+  FROM orders
+  WHERE id = $1
+  `;
+  const {
+    rows: [order],
+  } = await db.query(sql, [id]);
+  return order;
+}
+
+export async function getOrdersByProductId(id) {
+  const sql = `
+  SELECT orders.*
+  FROM
+    orders
+    JOIN orders_products ON orders.id = orders_products.order_id
+  WHERE orders_products.product_id = $1
+  `;
+  const { rows: orders } = await db.query(sql, [id]);
+  return orders;
+}
--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,31 @@
-DROP TABLE IF EXISTS order_products;
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS orders_products;
 DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS users;
 
-
 CREATE TABLE users (
-  id serial PRIMARY KEY,
-  username text UNIQUE NOT NULL,
-  password text NOT NULL
-);
-
-CREATE TABLE orders (
-  id serial PRIMARY KEY,
-  name text NOT NULL,
-  note text,
-  user_id integer NOT NULL
- );
-
-CREATE TABLE order_products (
-  order_id integer NOT NULL,
-  product_id integer NOT NULL,
-  quanity integer NOT NULL
-  order_id integer NOT NULL REFERENCES order(id) ON DELETE CASCADE,
-  product_id integer NOT NULL REFERENCES product(id) ON DELETE CASCADE
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
 );
 
 CREATE TABLE products (
-  id serial PRIMARY KEY,
-  title text NOT NULL,
-  price decimal NOT NULL
-  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  price DECIMAL NOT NULL
+);
 
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  date DATE NOT NULL,
+  note TEXT,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE orders_products (
+  order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL,
+  PRIMARY KEY (order_id, product_id)
 );
